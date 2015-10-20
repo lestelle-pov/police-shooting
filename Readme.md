a79ff47caf384bb38d6e94f6b0bf18072e777e06
Kirk Lestelle <kirklest@uw.edu>
2015-10-19 21:28:53 -0700

    Map is working

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/map-challenge).
+
+The web page is hosted [here](http://students.washington.edu/kirklest/info343/police-shooting)
--- js/map.js
+++ js/map.js
@@ -1,27 +1,25 @@
 // Function to draw your map
 var drawMap = function() {
-
-  // Create map and set view
- 
-
-  // Create a tile layer variable using the appropriate url
-
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+	// Create map and set view
+	var map = L.map('map-container').setView([38.7, -98.35], 4);
+	// Create a tile layer variable using the appropriate url
+	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 15});
+	// Add the layer to your map
+ 	layer.addTo(map);
+	// Execute your function to get data
+ 	getData(map);
 }
 
 // Function for getting data
-var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+var getData = function(map) {
+  	// Execute an AJAX request to get the data in data/response.js
+  	$.ajax({
+		url: './data/response.json',
+  		dataType: 'json',
+  		success: function(data) {
+  			console.log(data);
+  		}
+  	});
 }
 
 // Loop through your data and add the appropriate layers and points
